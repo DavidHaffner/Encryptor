a4c06ffa1abc92b754ba0a7f69dd3be3d48ecbba
ShangTzu <ShangTzu@LAPTOP-6VAODDHT.lan>
2018-03-06 14:25:20 +0100

    dopsán Client přímo v Javě, ale zatím nefunkční -> pro konzultaci

--- src/Application/Client.java
+++ src/Application/Client.java
@@ -14,19 +14,21 @@ public class Client extends ParseMessage {
 
     Client(String host, int port) {
         try {
-            sock = new Socket();
-            sock.connect(new InetSocketAddress(host, port));
-            out = sock.getOutputStream();
+            // nastavení socketu                                        // 1. ř.31: out.flush() -> účel?
+            sock = new Socket();                                        // 2. proč to správně nekomunikuje se Serverem? když na serveru více \n, bude fungovat? jinak ošetřit?
+            sock.connect(new InetSocketAddress(host, port));            // 3. kdyby socket byl na jiný server, tak do host -> IP adresa?
+            out = sock.getOutputStream();                               // 4. zadání na ten další úkol (řazení/streamy)?
             in = sock.getInputStream();
-            write("user\n");
-            read("", "Password:");
-            System.out.println("Password prompt readed");
-            write("heslo\n");
-            read("", "Welcome\n");
-            write("time\n");
+
+            // vlastní průběh komunikace
+            // podle šablony z ClientHandleru zadá 1/E/skakal pes pres oves + ukončí spojení
+            System.out.println(read("", "\n"));
+            write("1/E/skakal pes pres oves\n");
+
+            System.out.println(read("", "\n"));
+            write("E\n");
+
             out.flush();
-            System.out.println("Time on server is " + read("time is:", "\n"));
-            write("bye\n");
             sock.shutdownOutput();
             sock.close();
             System.out.println("Communication END");
@@ -38,7 +40,7 @@ public class Client extends ParseMessage {
     public static void main(String[] args) {
         Client c = new Client(
                 args.length > 0 ? args[0] : "localhost",
-                args.length > 1 ? Integer.parseInt(args[1]) : 8080
+                args.length > 1 ? Integer.parseInt(args[1]) : 8081
         );
     }
 }
